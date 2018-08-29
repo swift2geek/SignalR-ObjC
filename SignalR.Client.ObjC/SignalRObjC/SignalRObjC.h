5f1bfa3d1f060c361885097f8dce55e46e2b074d
Vladimir <swift2geek@gmail.com>
2018-08-29 19:23:33 +0300

    add new target - try2

---
+++ SignalR.Client.ObjC/SignalRObjC/SignalRObjC.h
@@ -0,0 +1,19 @@
+//
+//  SignalRObjC.h
+//  SignalRObjC
+//
+//  Created by Vladimir Valter on 29/08/2018.
+//  Copyright © 2018 DyKnow LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SignalRObjC.
+FOUNDATION_EXPORT double SignalRObjCVersionNumber;
+
+//! Project version string for SignalRObjC.
+FOUNDATION_EXPORT const unsigned char SignalRObjCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SignalRObjC/PublicHeader.h>
+
+
---
+++ SignalR.Client.ObjC/SignalRObjCTests/SignalRObjCTests.swift
@@ -0,0 +1,36 @@
+//
+//  SignalRObjCTests.swift
+//  SignalRObjCTests
+//
+//  Created by Vladimir Valter on 29/08/2018.
+//  Copyright © 2018 DyKnow LLC. All rights reserved.
+//
+
+import XCTest
+@testable import SignalRObjC
+
+class SignalRObjCTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
